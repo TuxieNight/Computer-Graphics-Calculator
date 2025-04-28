26306e24d5d7635301a90caf413be5a85ceebea2
TuxieNight <phowardsmith@wisc.edu>
2025-04-28 17:52:37 -0500

    Completed Basis Function

--- computeBasisFunction.js
+++ computeBasisFunction.js
@@ -13,6 +13,15 @@ let point_divider = document.getElementById("basisControlPoints");
 // get the button element
 let basisButton = document.getElementById("getBasisUvalue");
 
+// get result element
+let point_result_divider = document.getElementById("basisPointResults");
+
+// get divider for function results
+let func_result_divider = document.getElementById("basisFunctionResults");
+
+// begin by updating the points with some default
+updatePoints();
+
 // if numFuncsEle is changed, update the points
 numFuncsEle.onchange = function() {
     updatePoints();
@@ -70,6 +79,9 @@ function compute() {
     // Get the u value
     let u_value = parseFraction(u_ele.value);
 
+    // store values for each function
+    let funcValues = [];
+
     // pass u_value to readFunc for each function
     for (let i = 0; i < numFuncs; i++) {
         // get the function text from the input field
@@ -78,7 +90,36 @@ function compute() {
         // Replace 'u' with its value in the function text (g means all instances)
         let result = evaluateMathExpression(funcText, u_value);
 
-        console.log(`Result for function ${i}:`, result);
+        funcValues.push(result);
+    }
+
+    // clear the function results
+    func_result_divider.innerHTML = "";
+
+    // create the function results
+    for(let i = 0; i < numFuncs; i++) {
+        // create the function result elements
+        func_result_divider.innerHTML += "<b>Function " + (i + 1) + ":</b> " + document.getElementById("func_" + i).value + " = " + funcValues[i] + "<br>";
+    }
+
+    // determine the values using the control points and function values
+    for (let i = 0; i < sizePoints; i++) {
+        // value for row i
+        let value = 0;
+
+        // sum the value at element j for each point set i
+        for (let j = 0; j < numFuncs; j++) {
+            // get function j
+            let funcValue = funcValues[j];
+
+            // function j multiplies to everything in point set j
+            // result is the sum of all of the point i elements multiplied by their function j
+            value += funcValue * controlPoints[j][i];
+        }
+
+        // store the value in the result element
+        let result_ele = document.getElementById("result_" + i);
+        result_ele.value = value;
     }
 }
 
@@ -110,9 +151,10 @@ function evaluateMathExpression(expression, u_value) {
 }
 
 function updatePoints() {
-    // clear all dividers
+    // clear all dividers with input elements
     func_divider.innerHTML = "";
     point_divider.innerHTML = "";
+    point_result_divider.innerHTML = "";
 
     // get the number of funcsions and points
     let numFuncs = parseFloat(numFuncsEle.value);
@@ -120,7 +162,7 @@ function updatePoints() {
     let sizePoints = parseFloat(pointsSizeEle.value);
 
 
-    // create the points for each divider
+    // create the points for each  input divider
     for (let i = 0; i < numFuncs; i++) {
         // create the function input elements
         let func_ele = document.createElement("input");
@@ -135,11 +177,17 @@ function updatePoints() {
         func_divider.appendChild(func_ele);
         func_divider.appendChild(document.createTextNode(" "));
 
-        // create break between functions
+        // create break between functions and results
         func_divider.appendChild(document.createElement("br"));
 
         // make the element longer
         func_ele.size = 60;
+
+        // create divider for each set of control points
+        let point_div = document.createElement("div");
+        point_div.id = "point_divider_" + i;
+
+        point_divider.appendChild(point_div);       
 
         // create the control point input elements
         for (let j = 0; j < sizePoints; j++) {
@@ -148,16 +196,44 @@ function updatePoints() {
             // set the type
             point_ele.type = "text";
 
+            // make the element smaller
+            point_ele.size = 10;
+
             // set the id
             point_ele.id = "point_" + i + "_" + j;
+            // set placeholder
+            point_ele.placeholder = "Point " + (i + 1);
 
-            // add elements and spaces
-            point_divider.appendChild(point_ele);
-            point_divider.appendChild(document.createTextNode(" "));
+            // add elements and spaces to the divider for this set of control points
+            point_div.appendChild(point_ele);
+            point_div.appendChild(document.createTextNode(" "));
+            // create break between points
+            point_div.appendChild(document.createElement("br"));
         }
-        // create break between points
-        point_divider.appendChild(document.createElement("br"));
     }
+
+        // create the points for each result divider
+    for (let i = 0; i < sizePoints; i++) {
+        // create result input elements
+        let result_ele = document.createElement("input");
+
+        // set the type
+        result_ele.type = "text";
+
+        // set the id
+        result_ele.id = "result_" + i;
+
+        // make results read only
+        result_ele.readOnly = true;
+
+        // add elements and spaces
+        point_result_divider.appendChild(result_ele);
+        point_result_divider.appendChild(document.createTextNode(" "));
+
+        // create break between functions and results
+        point_result_divider.appendChild(document.createElement("br"));
+    }
+
 }
 
 function parseFraction(number) {
